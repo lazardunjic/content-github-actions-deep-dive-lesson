34dbf50f752d2e61d9d1c5e321480a38f431fee1
Lazar Dunjić <131361860+lazardunjic@users.noreply.github.com>
2026-09-26 22:22:38 +0200

    Fix Python syntax in Lambda function so lint passes

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -1,5 +1,6 @@
 from github import Github
 
+
 def lambda_handler(event, context):
     """Lambda function wrapper
     Args:
@@ -8,12 +9,9 @@ def lambda_handler(event, context):
     Returns:
         string: greeting response
     """
-    print('Starting functions\n---------------------------------------------'
+    print('Starting functions\n---------------------------------------------')
 
     if event["input"] == "Hello":
-
         return "World"
 
-    else:
-
-        raise
+    raise ValueError("Unexpected input")
